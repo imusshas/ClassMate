7d9a3c08d72996afa79b24edab95555543ab3e39
Md. Abid Ullah Muhib <usshas@Mds-MacBook-Pro.local>
2024-02-19 15:28:30 +0600

    Updaded navigation version from 2.7.6 to 2.7.7

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -82,7 +82,7 @@ dependencies {
     kspTest ("com.google.dagger:hilt-compiler:2.50")
 
     //Navigation
-    implementation("androidx.navigation:navigation-compose:2.7.6")
+    implementation("androidx.navigation:navigation-compose:2.7.7")
     // For Collecting States From View Model
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.7.0")
 
