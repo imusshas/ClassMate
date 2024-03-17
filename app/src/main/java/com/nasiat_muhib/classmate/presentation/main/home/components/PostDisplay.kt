5a11efeeb10e2b7f3fb9ddc83f33a8a9cfe89a80
Md. Abid Ullah Muhib <usshas@Mds-MacBook-Pro.local>
2024-03-17 20:41:57 +0600

    Fixed date/time format

--- app/src/main/java/com/nasiat_muhib/classmate/presentation/main/home/components/PostDisplay.kt
+++ app/src/main/java/com/nasiat_muhib/classmate/presentation/main/home/components/PostDisplay.kt
@@ -1,22 +1,18 @@
 package com.nasiat_muhib.classmate.presentation.main.home.components
 
+import android.icu.text.SimpleDateFormat
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.shrinkHorizontally
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.CheckCircle
 import androidx.compose.material.icons.filled.Delete
-import androidx.compose.material.icons.filled.SmartDisplay
 import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
@@ -30,23 +26,22 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.text.font.FontWeight
-import com.nasiat_muhib.classmate.data.model.Course
 import com.nasiat_muhib.classmate.data.model.Post
 import com.nasiat_muhib.classmate.domain.event.HomeUIEvent
-import com.nasiat_muhib.classmate.navigation.Screen
 import com.nasiat_muhib.classmate.presentation.main.home.HomeViewModel
-import com.nasiat_muhib.classmate.ui.theme.ExtraExtraSmallSpace
+import com.nasiat_muhib.classmate.strings.AM
+import com.nasiat_muhib.classmate.strings.PM
 import com.nasiat_muhib.classmate.ui.theme.ExtraSmallHeight
 import com.nasiat_muhib.classmate.ui.theme.LargeHeight
 import com.nasiat_muhib.classmate.ui.theme.LargeRounded
 import com.nasiat_muhib.classmate.ui.theme.LargeSpace
 import com.nasiat_muhib.classmate.ui.theme.MediumSpace
-import com.nasiat_muhib.classmate.ui.theme.NormalHeight
 import com.nasiat_muhib.classmate.ui.theme.PostHeight
-import com.nasiat_muhib.classmate.ui.theme.SmallRounded
 import com.nasiat_muhib.classmate.ui.theme.SmallSpace
 import me.saket.swipe.SwipeAction
 import me.saket.swipe.SwipeableActionsBox
+import java.util.Date
+import java.util.Locale
 
 @Composable
 fun PostDisplay(
@@ -95,10 +90,27 @@ fun PostDisplay(
                     Column(modifier = Modifier
                         .fillMaxSize()
                         .padding(SmallSpace)) {
+                        val date = Date(post.timestamp)
+                        val dateFormatter = SimpleDateFormat("dd-MM-yyyy", Locale.getDefault())
+                        val dateString = dateFormatter.format(date)
+
+                        val timeFormatter = SimpleDateFormat("HH:mm", Locale.getDefault())
+                        val timeString = timeFormatter.format(date)
+
+                        val hours = timeString.substringBefore(":").toInt()
+                        val hour = if (hours > 12) hours - 12 else if (hours == 0) 12 else hours
+
+                        val minute = timeString.substringAfter(":").toInt()
+                        val timeFormatterForSecond = SimpleDateFormat("HH:mm:ss", Locale.getDefault())
+                        val timeStringForSecond = timeFormatter.format(date)
+                        val second = timeStringForSecond.substringAfterLast(":").toInt()
+
+                        val shift = if (hours < 12) AM else PM
                         Text(text = "${post.firstName} ${post.firstName}", fontWeight = FontWeight.Black)
-                        Text(text = post.timestamp.toString(), fontWeight = FontWeight.Light)
+                        Text(text = dateString, fontWeight = FontWeight.Light)
+                        Text(text = "$hour:$minute:$second $shift", fontWeight = FontWeight.Light)
                         Spacer(modifier = Modifier.height(LargeSpace))
-                        Text(text = post.courseCode, style = MaterialTheme.typography.titleMedium)
+                        Text(text = post.courseCode, style = MaterialTheme.typography.titleMedium, fontWeight = FontWeight.Bold)
                         Text(text = post.description)
                     }
                 }
