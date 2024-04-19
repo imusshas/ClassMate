c37b1509d0d910ea3217f89d45f22b70be6e87a4
Md. Abid Ullah Muhib <usshas@Mds-MacBook-Pro.local>
2024-04-19 17:05:41 +0600

    Customized theme by disabling dynamic color

--- app/src/main/java/com/nasiat_muhib/classmate/ui/theme/Theme.kt
+++ app/src/main/java/com/nasiat_muhib/classmate/ui/theme/Theme.kt
@@ -25,9 +25,9 @@ private val DarkColorScheme = darkColorScheme(
 private val LightColorScheme = lightColorScheme(
         primary = PrimaryRed,
         secondary = PurpleGrey40,
-        tertiary = Pink40
+        tertiary = Pink40,
 
-        /* Other default colors to override
+        // Other default colors to override
     background = Color(0xFFFFFBFE),
     surface = Color(0xFFFFFBFE),
     onPrimary = Color.White,
@@ -35,14 +35,14 @@ private val LightColorScheme = lightColorScheme(
     onTertiary = Color.White,
     onBackground = Color(0xFF1C1B1F),
     onSurface = Color(0xFF1C1B1F),
-    */
+
 )
 
 @Composable
 fun ClassMateTheme(
         darkTheme: Boolean = isSystemInDarkTheme(),
         // Dynamic color is available on Android 12+
-        dynamicColor: Boolean = true,
+        dynamicColor: Boolean = false,
         content: @Composable () -> Unit
 ) {
     val colorScheme = when {
